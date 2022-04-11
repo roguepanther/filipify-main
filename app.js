7ed3d9512aad601c163bfa8d1a18c1f1671b9e16
roguepanther <roguepanthernoob@gmail.com>
2022-04-11 14:26:15 +0100

    Added Bootstrap cards

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var headerName = document.querySelector("#headerName");
 var headerTitle = document.querySelector("#headerTitle");
 
 $(document).ready(function(){ 
-    $('h1').css('display', 'none');
-    $('h2').css('display', 'none');
+    $("#headerName").css('display', 'none');
+    $("#headerTitle").css('display', 'none');
     $("#headerName").fadeIn(3000);
     $("#headerTitle").fadeIn(3000);
 })
