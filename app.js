96bf0f6be8ae3875c0f93b453bb2fa8f3355eab5
roguepanther <roguepanthernoob@gmail.com>
2022-04-11 12:38:15 +0100

    Added jQuery with FadeIn

---
+++ app.js
@@ -0,0 +1,9 @@
+var headerName = document.querySelector("#headerName");
+var headerTitle = document.querySelector("#headerTitle");
+
+$(document).ready(function(){ 
+    $('h1').css('display', 'none');
+    $('h2').css('display', 'none');
+    $("#headerName").fadeIn(3000);
+    $("#headerTitle").fadeIn(3000);
+})
